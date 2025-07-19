a1ca1e9dad5ff54be7a5c209040aa87b238e9d2a
Gabzmelo <gabimelo.pp@gmail.com>
2025-07-19 17:25:01 -0300

    Adiciona rotas

--- tech-interview-backend-entry-level-main/config/routes.rb
+++ tech-interview-backend-entry-level-main/config/routes.rb
@@ -1,15 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
-
-  resources :products
-
-  get    "/cart",             to: "carts#show"
-  post   "/cart",             to: "carts#create"
-  post   "/cart/add_item",    to: "carts#add_item"
-  delete "/cart/:product_id", to: "carts#destroy"
-
-  get "up" => "rails/health#show", as: :rails_health_check
-  root "rails/health#show"
+  resources :carts, only: [:create, :show, :destroy] do
+    member do
+      patch :update_item_quantity
+      delete 'remove_item/:product_id', to: 'carts#destroy_item'
+    end
+  end
 end
+
